3949bab774b0b698b53b29cfb30457c604f2e48e
meta112 <ryan.cj.zhu@gmail.com>
2024-01-17 23:10:49 -0500

    Initialize databases

---
+++ dbInit.js
@@ -0,0 +1,86 @@
+// run this once to initialize the database
+
+const Sequelize = require('sequelize');
+
+
+const sequelize = new Sequelize('database', 'user', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+    logging: false,
+    storage: 'database.sqlite',
+});
+
+module.exports = {
+    Users: sequelize.define('users', {
+        username: {
+            type: Sequelize.STRING,
+            unique: true,
+        },
+        active_profile: {
+            type: Sequelize.INTEGER,
+            defaultValue: 1,
+            allowNull: false,
+        },
+        primogems: {
+            type: Sequelize.INTEGER,
+            defaultValue: 0,
+            allowNull: false,
+        },
+        starglitter: {
+            type: Sequelize.INTEGER,
+            defaultValue: 0,
+            allowNull: false,
+        },
+        fates: {
+            type: Sequelize.INTEGER,
+            defaultValue: 0,
+            allowNull: false,
+        },
+        pity: {
+            type: Sequelize.INTEGER,
+            defaultValue: 0,
+            allowNull: false,
+        },
+        guaranteed: {
+            type: Sequelize.BOOLEAN,
+            defaultValue: false,
+        },
+    }),
+    Wishlist: sequelize.define('wishlist', {
+        username: {
+            type: Sequelize.STRING,
+            unique: true,
+        },
+        profile: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+        },
+        target: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        versions_away: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+        },
+        firsthalf: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false,
+            defaultValue: true,
+        },
+        date: {
+            type: Sequelize.DATE,
+            allowNull: false,
+        },
+    }),
+    Versions: sequelize.define('versions', {
+        version: {
+            type: Sequelize.STRING,
+            unique: true,
+        },
+        date: {
+            type: Sequelize.DATE,
+            unique: true,
+        },
+    }),
+};
--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,14 @@
 const { Events } = require('discord.js');
 
+const Tables = require ('../dbInit.js');
+
 module.exports = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
+        Tables.Users.sync();
+        Tables.Versions.sync();
+        Tables.Wishlist.sync();
         console.log(`Ready! Logged in as ${client.user.tag}`);
     },
 };
