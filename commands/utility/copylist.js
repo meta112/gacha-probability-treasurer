545699422399a5158bf7d9b54d07d8163f5ede71
meta112 <ryan.cj.zhu@gmail.com>
2024-03-16 18:51:15 -0400

    Add warning in copylist if invalid profiles chosen

--- commands/utility/copylist.js
+++ commands/utility/copylist.js
@@ -5,18 +5,23 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('copylist')
         .setDescription('Copy a profile into another profile')
-        .addStringOption(option =>
+        .addIntegerOption(option =>
             option
                 .setName('dest')
                 .setDescription('Wishlist profile to overwrite')
                 .setRequired(true))
-        .addStringOption(option =>
+        .addIntegerOption(option =>
             option
                 .setName('src')
                 .setDescription('Wishlist profile to copy from')
                 .setRequired(true)),
     async execute(interaction) {
-
+        const dest = interaction.options.getInteger('dest');
+        const src = interaction.options.getInteger('src');
+        if (dest <= 0 || dest > 3 || src <= 0 || src > 3){
+            return interaction.reply({ content: 'Must be between 1 and 3', ephemeral: true });
+        }
+        if (dest == src) return interaction.reply({ content: 'Must input different numbers', ephemeral: true });
         return interaction.reply('');
     },
 };
