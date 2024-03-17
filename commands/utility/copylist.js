7f400625cb80125d563283dbc92a689c9f82c5d5
meta112 <ryan.cj.zhu@gmail.com>
2024-03-17 19:06:52 -0400

    Implement copylist command

--- commands/utility/copylist.js
+++ commands/utility/copylist.js
@@ -22,6 +22,28 @@ module.exports = {
             return interaction.reply({ content: 'Must be between 1 and 3', ephemeral: true });
         }
         if (dest == src) return interaction.reply({ content: 'Must input different numbers', ephemeral: true });
-        return interaction.reply('');
+
+        await Tables.Wishlist.destroy({ where: { username: interaction.user.username, profile: dest } });
+        const chars = await Tables.Wishlist.findAll({ where: { username: interaction.user.username, profile: src } });
+        try {
+            console.log(`entering try: ${chars}`);
+            for (let i = 0; i < chars.length; i++){
+                console.log(i);
+                const c = chars[i];
+                const newchar = await Tables.Wishlist.create({
+                    username: interaction.user.username,
+                    profile: dest,
+                    target: c.target,
+                    cons: c.cons,
+                    versions_away: c.versions_away,
+                    firsthalf: c.firsthalf,
+                    version: c.version,
+                });
+                console.log(`Copying ${c.target} as ${newchar.target}`);
+            }
+            return interaction.reply(`Copied Wishlist ${src} into Wishlist ${dest}.`);
+        } catch (e) {
+            return interaction.reply('Something went wrong with updating wishlist.');
+        }
     },
 };
